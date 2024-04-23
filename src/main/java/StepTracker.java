160d405c9b04ff6cd0d2da6a4ce94bcb9f5ef6b9
Eric Zhao <140433074+mykinglebron@users.noreply.github.com>
2024-04-22 20:33:26 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,31 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int minSteps, totalSteps;
+ private int totalDays, actDays;
+ public StepTracker(int goals){
+minSteps=goals;
+totalSteps=0;
+ totalDays=0;
+  actDays=0;
+
+ }
+ public void addDailySteps(int steps){
+  totalSteps= totalSteps +steps;
+  totalDays++;
+  if(steps>= minSteps)
+   actDays++;
+ }
+ public int activeDays(){
+return actDays;
+ }
+
+public double averageSteps(){
+if(totalDays==0.0){
+return 0.0;}
+ else{
+return (double) totalStpes/totalDays;
+ }
+
+}
 } 
